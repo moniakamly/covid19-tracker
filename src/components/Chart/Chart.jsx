554af57c35ea1cc66ba3944b2bc37437934f05ed
AYMEN <moniakamli95@gmail.com>
2021-05-10 16:53:26 +0200

    implement the cards and fetch data from the api

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const Chart = () => {
+    return (
+        <h1>Chart</h1>
+    )
+}
+
+export default Chart;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const CountryPicker = () => {
+    return (
+        <h1>CountryPicker</h1>
+    )
+}
+
+export default CountryPicker;
